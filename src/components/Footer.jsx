80f30430f89968e314ebc5307a0e793d4647ba55
Mazin-Ayman <107291934+Mazin-Ayman@users.noreply.github.com>
2023-06-24 14:38:31 +0300

    Update Footer.jsx
    anythind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return(
     <footer className="footer">
-      Ayman Ahmed Abdelaziz internal control
+      Ayman Ahmed AbdelAziz  Fayoum Internal Control
     </footer>
   );
 }
