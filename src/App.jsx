ae0552a302249ddea5ea99c05d6859e8e8419f4b
mazin-ayman <mazinbinayman0807@gmail.com>
2023-06-27 00:32:50 +0300

    I added the new navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import One from './components/One';
 import Two from './components/Two';
 import Three from './components/Three';
 import Navbar from './components/Navbar';
+import Navbar2 from './components/Navbar2';
 import Footer from './components/Footer';
 
 function App() {
 
   return (
     <div className='app'>
-      <Navbar />
+      <Navbar2 />
       <Routes>
         <Route path='/' element={<One />} />
         <Route path='/code' element={<Two />} />
---
+++ src/components/Navbar2.jsx
@@ -0,0 +1,45 @@
+import { Link, useNavigate } from "react-router-dom";
+
+import { useRef, useState } from 'react';
+
+import './navbar2.css';
+
+const Navbar2 = () => {
+  const [link, setLink] = useState('one');
+  const navigate = useNavigate();
+  const menuRef = useRef();
+  const linksRef = useRef();
+
+  const handleClick = () => {
+    [menuRef, linksRef].map(one => one.current.classList.toggle('active'))
+  }
+  const linksClick = (linkName) => {
+    setLink(linkName);
+    [menuRef, linksRef].map(one => one.current.classList.toggle('active'));
+  }
+
+  return (
+    <header className="navbar2">
+      <nav>
+        <div className="mine" ref={menuRef} onClick={handleClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="logo" onClick={() => {
+          navigate('/')
+          setLink('one')
+        }}>ABE</div>
+      </nav>
+      <div className="links" ref={linksRef}>
+        <ul>
+          <li className={link === 'one' ? 'on' : ''}><Link to='/' onClick={(e) => linksClick('one')}>درجة التقييم</Link></li>
+          <li className={link === 'two' ? 'on' : ''}><Link to='/code' onClick={(e) => linksClick('two')}>أكواد البنوك</Link></li>
+          <li className={link === 'three' ? 'on' : ''}><Link to='/short' onClick={(e) => linksClick('three')}>اختصارات الـ Finacle</Link></li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar2;
