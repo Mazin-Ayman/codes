a4931ab20f28d775911917f22a2db3e6f8a08310
Mazin-Ayman <mazinbinayman0807@gmail.com>
2023-07-13 17:14:06 +0300

    Some updates

--- src/components/Four.jsx
+++ src/components/Four.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import { Autocomplete, Stack, TextField } from '@mui/material';
-import data from './../../data3';
-
-
+import { useState } from "react";
+import { Autocomplete, Stack, TextField } from "@mui/material";
+import data from "./../../data3";
 
 const Four = () => {
-  const [season, setSeason] = useState('');
-  const [crop, setCrop] = useState('');
-
+  const [season, setSeason] = useState("");
+  const [crop, setCrop] = useState("");
   const [trVal, setTrVal] = useState(null);
+  const [trValInd, setTrValInd] = useState(null);
+  const [trValMinValCor, setTrValMinValCor] = useState(null);
+  const [maxValCor, setMaxValCor] = useState(null);
   const [modVal, setModVal] = useState(null);
   const [begin, setBegin] = useState(null);
   const [end, setEnd] = useState(null);
@@ -26,110 +26,171 @@ const Four = () => {
     setInstNumber(data[season][crop].inst_no);
     setInstDate(data[season][crop].inst_dte);
     setNotes(data[season][crop].notes);
-  }
+    setTrValInd(data[season][crop].tr_va_ind);
+    setTrValMinValCor(data[season][crop].min_val_cor);
+    setMaxValCor(data[season][crop].max_val_cor);
+  };
 
   return (
     <main className="four">
       <form onSubmit={(e) => e.target.preventDefault()}>
-        <Stack spacing={3} sx={{ width: '100%' }}>
+        <Stack spacing={3} sx={{ width: "100%" }}>
           <Autocomplete
             // disablePortal
             id="season-inp"
             options={Object.keys(data)}
             sx={{
-              width: 250, backgroundColor: "#fff", borderRadius: 1, '& input': { textAlign: 'center' }, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
+              width: 250,
+              backgroundColor: "#fff",
+              borderRadius: 1,
+              "& input": { textAlign: "center" },
+              boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
               "& .MuiAutocomplete-popupIndicator": {
                 color: "tan",
               },
-              "& *": { fontFamily: 'Cairo' },
+              "& *": { fontFamily: "Cairo" },
             }}
             inputValue={season}
             onInputChange={(e, v, r) => {
               if (v) {
                 setSeason(v);
-                console.log(e, r, v)
+                console.log(e, r, v);
               } else {
-                console.log("Error accured")
+                console.log("Error accured");
               }
             }}
-            onDoubleClick={() => setSeason('')}
+            onDoubleClick={() => setSeason("")}
             // onSelect={}
             onKeyDown={(e) => {
-              if (e.key === 'Enter') e.preventDefault();
+              if (e.key === "Enter") e.preventDefault();
             }}
-            renderInput={(params) => <TextField {...params} label="الفئة والموسم" sx={{ textAlign: 'center' }} />}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="الفئة والموسم"
+                sx={{ textAlign: "center" }}
+              />
+            )}
           />
 
-          {(season && season !== "" && season != null) && <Autocomplete
-            disablePortal
-            id="crop-inp"
-            options={Object.keys(data[`${season}`])}
-            sx={{
-              width: 250, backgroundColor: "#fff", borderRadius: 1, '& input': { textAlign: 'center' }, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px',
-              "& .MuiAutocomplete-popupIndicator": {
-                color: "tan",
-              }
-            }}
-            inputValue={crop}
-            onInputChange={(e, v) => {
-              setCrop(v.toString())
-            }}
-            onBlur={handler}
-            onDoubleClick={() => setCrop('')}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') e.preventDefault();
-            }}
-            onSelect={handler}
-            renderInput={(params) => <TextField {...params} label="المحصول" sx={{ textAlign: 'center' }} />}
-          />}
-
+          {season && season !== "" && season != null && (
+            <Autocomplete
+              disablePortal
+              id="crop-inp"
+              options={season in data && Object.keys(data[`${season}`])}
+              sx={{
+                width: 250,
+                backgroundColor: "#fff",
+                borderRadius: 1,
+                "& input": { textAlign: "center" },
+                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+                "& .MuiAutocomplete-popupIndicator": {
+                  color: "tan",
+                },
+              }}
+              inputValue={crop}
+              onInputChange={(e, v) => {
+                setCrop(v.toString());
+              }}
+              onBlur={handler}
+              onDoubleClick={() => setCrop("")}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") e.preventDefault();
+              }}
+              onSelect={handler}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="المحصول"
+                  sx={{ textAlign: "center" }}
+                />
+              )}
+            />
+          )}
         </Stack>
       </form>
-      {(crop && crop !== '') && <div className='data'>
+      {crop && crop !== "" && (
+        <div className="data">
+          {/* {()} */}
+          {trVal && trVal != null && (
+            <div className="tr-val">
+              <span className="data-item-title">ري عادي:</span>
+              <span className="data-item-value">{trVal}</span>
+            </div>
+          )}
 
-        {(trVal && trVal != null) && <div className='tr-val'>
-          <span className='data-item-title'>ري عادي:</span>
-          <span className='data-item-value'>{trVal}</span>
-        </div>}
+          {modVal && modVal != null && (
+            <div className="mod-val">
+              <span className="data-item-title">ري حديث:</span>
+              <span className="data-item-value">{modVal}</span>
+            </div>
+          )}
+          {trValInd && trValInd != null && (
+            <div className="tr-val-ind">
+              <span className="data-item-title">ري عادي أفراد:</span>
+              <span className="data-item-value">{trValInd}</span>
+            </div>
+          )}
 
-        {(modVal && modVal != null) && <div className='mod-val'>
-          <span className='data-item-title'>ري حديث:</span>
-          <span className='data-item-value'>{modVal}</span>
-        </div>}
+          {trValMinValCor && trValMinValCor != null && (
+            <div className="tr-val-ind">
+              <span className="data-item-title">حد أدنى عادي شركات:</span>
+              <span className="data-item-value">{trValMinValCor}</span>
+            </div>
+          )}
 
-        {(begin && begin != null) && <div className='begin-date'>
-          <span className='data-item-title'>بداية المنح:</span>
-          <span className='data-item-value'>{begin}</span>
-        </div>}
+          {maxValCor && maxValCor != null && (
+            <div className="tr-val-ind">
+              <span className="data-item-title">حد أقصى عادي شركات:</span>
+              <span className="data-item-value">{maxValCor}</span>
+            </div>
+          )}
 
-        {(end && end != null) && <div className='end-date'>
-          <span className='data-item-title'>نهاية المنح:</span>
-          <span className='data-item-value'>{end}</span>
-        </div>}
+          {begin && begin != null && (
+            <div className="begin-date">
+              <span className="data-item-title">بداية المنح:</span>
+              <span className="data-item-value">{begin}</span>
+            </div>
+          )}
 
-        {(due && due != null) && <div className='due'>
-          <span className='data-item-title'>تاريخ الاستحقاق:</span>
-          <span className='data-item-value'>{due}</span>
-        </div>}
+          {end && end != null && (
+            <div className="end-date">
+              <span className="data-item-title">نهاية المنح:</span>
+              <span className="data-item-value">{end}</span>
+            </div>
+          )}
 
-        {(instNumber && instNumber != null) && <div className='inst-number'>
-          <span className='data-item-title'>كتاب دوري رقم:</span>
-          <span className='data-item-value'>{instNumber}</span>
-        </div>}
+          {due && due != null && (
+            <div className="due">
+              <span className="data-item-title">تاريخ الاستحقاق:</span>
+              <span className="data-item-value">{due}</span>
+            </div>
+          )}
 
-        {(instDate && instDate != null) && <div className='inst-date'>
-          <span className='data-item-title'>بتاريخ:</span>
-          <span className='data-item-value'>{instDate}</span>
-        </div>}
+          {instNumber && instNumber != null && (
+            <div className="inst-number">
+              <span className="data-item-title">كتاب دوري رقم:</span>
+              <span className="data-item-value">{instNumber}</span>
+            </div>
+          )}
 
-        {(notes && notes != null) && <div className='notes'>
-          <span className='data-item-title'>ملاحظات:</span>
-          <span className='data-item-value'>{notes}</span>
-        </div>}
-      </div>}
-    </main >
+          {instDate && instDate != null && (
+            <div className="inst-date">
+              <span className="data-item-title">بتاريخ:</span>
+              <span className="data-item-value">{instDate}</span>
+            </div>
+          )}
+
+          {notes && notes != null && (
+            <div className="notes">
+              <span className="data-item-title">ملاحظات:</span>
+              <span className="data-item-value">{notes}</span>
+            </div>
+          )}
+        </div>
+      )}
+    </main>
   );
-}
-
+};
 
 export default Four;
--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -1,59 +1,74 @@
-import data from './../../data2';
-import { useState } from 'react';
-import { Autocomplete, Stack, TextField } from '@mui/material';
+import data from "./../../data2";
+import { useState } from "react";
+import { Autocomplete, Stack, TextField } from "@mui/material";
 const Three = () => {
-  const [id, setId] = useState('');
+  const [id, setId] = useState("");
   const [shortcut, setShortcut] = useState(null);
   const [jobEn, setJobEn] = useState(null);
   const [office, setOffice] = useState(null);
 
   const handler = () => {
-    // if (!id || id === "") {
-    //   return;
-    // };
-    setShortcut(data[id].shortcut)
-    setJobEn(data[id].en_job)
-    setOffice(data[id].officer)
-  }
+    setShortcut(data[id].shortcut);
+    setJobEn(data[id].en_job);
+    setOffice(data[id].officer);
+  };
   return (
     <main className="three">
       <form onSubmit={(e) => e.target.preventDefault()}>
-        <Stack sx={{ width: '100%' }}>
+        <Stack sx={{ width: "100%" }}>
           <Autocomplete
             disablePortal
             id="combo-box-demo"
             options={Object.keys(data)}
-            sx={{ width: 250, backgroundColor: "#fff", borderRadius: 1, '& input': { textAlign: 'center' }, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px' }}
+            sx={{
+              width: 250,
+              backgroundColor: "#fff",
+              borderRadius: 1,
+              "& input": { textAlign: "center" },
+              boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+            }}
             inputValue={id}
             onInputChange={(e, v) => {
-              setId(v.toString())
+              setId(v.toString());
             }}
             onBlur={handler}
-            onDoubleClick={() => setId('')}
+            onDoubleClick={() => setId("")}
             onKeyDown={(e) => {
-              if (e.key === 'Enter') e.preventDefault();
+              if (e.key === "Enter") e.preventDefault();
             }}
             onSelect={handler}
-            renderInput={(params) => <TextField {...params} label="اكتب الوظيفة لتعرف الاختصار" sx={{ textAlign: 'center' }} />}
-          // freeSolo
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="اكتب الوظيفة لتعرف الاختصار"
+                sx={{ textAlign: "center" }}
+              />
+            )}
+            // freeSolo
           />
         </Stack>
       </form>
-      <div className='data'>
-        {shortcut && <div>
-          <p>الإختصار:</p>
-          <p className='shrt'>{shortcut}</p>
-        </div>}
-        {jobEn && <div>
-          <p>الوظيفة:</p>
-          <p className='jben'>{jobEn}</p>
-        </div>}
-        {office && <div>
-          <p>المختص:</p>
-          <p className='off'>{office}</p>
-        </div>}
+      <div className="data">
+        {shortcut && (
+          <div>
+            <p>الإختصار:</p>
+            <p className="shrt">{shortcut}</p>
+          </div>
+        )}
+        {jobEn && (
+          <div>
+            <p>الوظيفة:</p>
+            <p className="jben">{jobEn}</p>
+          </div>
+        )}
+        {office && (
+          <div>
+            <p>المختص:</p>
+            <p className="off">{office}</p>
+          </div>
+        )}
       </div>
     </main>
   );
-}
+};
 export default Three;
