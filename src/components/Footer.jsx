54f5114e6bd6bcde5061af28a746132d07d9d620
ayman0579 <132126997+ayman0579@users.noreply.github.com>
2024-01-03 20:49:34 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return(
     <footer className="footer">
-      Ayman Ahmed AbdelAziz  Fayoum Internal Control-Last updated Jan 3rd 2024
+      Ayman Ahmed AbdelAziz  Fayoum Internal Control- Last Updated on 03/01/2024
     </footer>
   );
 }
