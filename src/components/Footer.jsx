bb8c0f2ef23c7dd48fac34031aae5423e59500ec
Mazin-Ayman <mazinbinayman0807@gmail.com>
2024-02-04 18:38:47 +0200

    some f**** chANGES

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
           fontWeight: 'bold', 
           color: '#fff'
         }
-      }>Ayman Ahmed AbdelAziz  Fayoum Internal Control- Last Updated on 04/02/2024</a>
+      }>Ayman Ahmed AbdelAziz  Fayoum Internal Control - Last Updated on 04/02/2024</a>
     </footer>
   );
 }
