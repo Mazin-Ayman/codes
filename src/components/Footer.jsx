d147a047de6ce11a9b5a2c347d0ead15121e97c2
ayman0579 <132126997+ayman0579@users.noreply.github.com>
2024-03-08 07:59:34 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         {
           textDecoration: "none",
           fontWeight: 'bold', 
-          color: 'Green',
+          color: 'Red',
         }
       }>Ayman Ahmed AbdelAziz  Fayoum Internal Control - Last Updated on 08/03/2024</a>
     </footer>
