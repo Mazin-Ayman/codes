ae8d8dedb126e37b68ee4d304b9c44ca8ffeca58
ayman0579 <132126997+ayman0579@users.noreply.github.com>
2024-09-22 06:51:14 +0300

    Update Footer.jsx
    Link updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return(
     <footer className="footer">
-      <a target="blank" href="https://drive.google.com/file/d/1xSwtF84O15xTkOvD58ILEIJvxjqi8UyR/view?usp=drivesdk" style={
+      <a target="blank" href="https://abe.com.eg/" style={
         {
           textDecoration: "none",
           fontWeight: 'bold', 
