e01418d5550b3b0aaa3402f2daaeb5d2e37af397
Mazin-Ayman <mazinbinayman0807@gmail.com>
2025-12-27 14:24:47 +0200

    update CropSeasonCalculator.jsx + added more entries in cropSeasons.js

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@ from openpyxl import load_workbook
 from pathlib import Path
 import json
 from datetime import datetime
+import openpyxl
 
 def conv(date_or_str: datetime | str, is_full: bool = False) -> str:
     if type(date_or_str) == datetime:
@@ -15,30 +16,51 @@ path = Path(r"C:/Users/ayman/OneDrive/Desktop/crops.xlsx")
 wb = load_workbook(filename=path, read_only=True)
 
 sheet = wb["cropFixed"]
-rng = sheet["A3":"J162"]
+rng = sheet["A3":"N176"]
 
 obj = {}
 
 seasons = [cell[0].value for cell in rng]
 seasons = list(dict.fromkeys(seasons).keys())
+# sheet.cell()
 
 for season in seasons:
     obj[season] = {}
 
 for row in rng:
-    season, crop, normal_irrigation, modern_irrigation, giving_start, giving_end, due_date, inst_number, inst_date, notes = (c.value for c in row)
-    # print(*row, sep="\n")
+    row = tuple(r for r in row if r.value != None)
+    # print(row[-1].value,len(row))
     # break
-    obj[season][crop] = {
-        "tr_val": normal_irrigation, 
-        "mod_val": modern_irrigation, 
-        "begin" : conv(giving_start), 
-        "end": conv(giving_end), 
-        "due": conv(due_date), 
-        "inst_no": inst_number,
-        "inst_dte": conv(inst_date, True),
-        "notes": notes
-    }
+    if len(row) <= 10:
+        season, crop, traditional_irrigation, modern_irrigation, giving_start, giving_end, due_date, inst_number, inst_date, notes = (c.value for c in row)
+
+        obj[season][crop] = {
+            "tr_val": traditional_irrigation, 
+            "mod_val": modern_irrigation, 
+            "begin" : conv(giving_start), 
+            "end": conv(giving_end), 
+            "due": conv(due_date), 
+            "inst_no": inst_number,
+            "inst_dte": conv(inst_date, True),
+            "notes": notes
+        }
+    else:
+        season, crop, one, two, three, four, five, six, giving_start, giving_end, due_date, inst_number, inst_date, notes = (c.value for c in row)
+        obj[season][crop] = {
+            "tr_ind_val": one,
+            "mod_ind_val": two,
+            "min_tr_comp_val": three,
+            "max_tr_comp_val": four,
+            "min_mod_comp_val": five,
+            "max_mod_comp_val": six,
+            "begin" : conv(giving_start), 
+            "end": conv(giving_end), 
+            "due": conv(due_date), 
+            "inst_no": inst_number,
+            "inst_dte": conv(inst_date, True),
+            "notes": notes
+        }
+
 
 with open("output/out.json", "w") as file:
     json.dump(obj, file, ensure_ascii=False, indent=2)
--- src/components/CropSeasonCalculator.jsx
+++ src/components/CropSeasonCalculator.jsx
@@ -7,10 +7,11 @@ import data from "../data/cropSeasons";
 const CropSeasonCalculator = () => {
   const [season, setSeason] = useState("");
   const [crop, setCrop] = useState("");
-
+  console.log(data);
   // Helper to safely get crop data
   const getCropData = () => {
     if (season && crop && data[season] && data[season][crop]) {
+
       return data[season][crop];
     }
     return null;
@@ -83,82 +84,152 @@ const CropSeasonCalculator = () => {
 
       {cropData && (
         <div className="data">
-          {cropData.tr_val && (
+          {(Object.keys(cropData).length > 10) ? (
+            <div className="more_info">
+              <div>
+                <span className="data-item-title">ري عادي افراد:</span>
+                <span className="data-item-value">{cropData.tr_ind_val}</span>
+              </div>
+              <div>
+                <span className="data-item-title">ري حديث افراد:</span>
+                <span className="data-item-value">{cropData.mod_ind_val}</span>
+              </div>
+              <div>
+                <span className="data-item-title">حد ادنى ري عادي شركات:</span>
+                <span className="data-item-value">{cropData.min_tr_comp_val}</span>
+              </div>
+              <div>
+                <span className="data-item-title">حد اقصى ري عادي شركات:</span>
+                <span className="data-item-value">{cropData.max_tr_comp_val}</span>
+              </div>
+              <div>
+                <span className="data-item-title">حد ادنى ري حديث  شركات:</span>
+                <span className="data-item-value">{cropData.min_mod_comp_val}</span>
+              </div>
+              <div>
+                <span className="data-item-title">حد اقصى ري حديث  شركات:</span>
+                <span className="data-item-value">{cropData.max_mod_comp_val}</span>
+              </div>
+              {cropData.begin && (
+                <div className="begin-date">
+                  <span className="data-item-title">بداية المنح:</span>
+                  <span className="data-item-value">{cropData.begin}</span>
+                </div>
+              )}
+
+              {cropData.end && (
+                <div className="end-date">
+                  <span className="data-item-title">نهاية المنح:</span>
+                  <span className="data-item-value">{cropData.end}</span>
+                </div>
+              )}
+
+              {cropData.due && (
+                <div className="due">
+                  <span className="data-item-title">تاريخ الاستحقاق:</span>
+                  <span className="data-item-value">{cropData.due}</span>
+                </div>
+              )}
+
+              {cropData.inst_no && (
+                <div className="inst-number">
+                  <span className="data-item-title">كتاب دوري رقم:</span>
+                  <span className="data-item-value">{cropData.inst_no}</span>
+                </div>
+              )}
+
+              {cropData.inst_dte && (
+                <div className="inst-date">
+                  <span className="data-item-title">بتاريخ:</span>
+                  <span className="data-item-value">{cropData.inst_dte}</span>
+                </div>
+              )}
+
+              {cropData.notes && (
+                <div className="notes">
+                  <span className="data-item-title">ملاحظات:</span>
+                  <span className="data-item-value">{cropData.notes}</span>
+                </div>
+              )}
+            </div>
+          ) : (<div>{cropData.tr_val && (
             <div className="tr-val">
               <span className="data-item-title">ري عادي:</span>
               <span className="data-item-value">{cropData.tr_val}</span>
             </div>
-          )}
+          )
+          }
 
-          {cropData.mod_val && (
-            <div className="mod-val">
-              <span className="data-item-title">ري حديث:</span>
-              <span className="data-item-value">{cropData.mod_val}</span>
-            </div>
-          )}
+            {cropData.mod_val && (
+              <div className="mod-val">
+                <span className="data-item-title">ري حديث:</span>
+                <span className="data-item-value">{cropData.mod_val}</span>
+              </div>
+            )}
 
-          {cropData.tr_va_ind && (
-            <div className="tr-val-ind">
-              <span className="data-item-title">ري عادي أفراد:</span>
-              <span className="data-item-value">{cropData.tr_va_ind}</span>
-            </div>
-          )}
+            {cropData.tr_va_ind && (
+              <div className="tr-val-ind">
+                <span className="data-item-title">ري عادي أفراد:</span>
+                <span className="data-item-value">{cropData.tr_va_ind}</span>
+              </div>
+            )}
 
-          {cropData.min_val_cor && (
-            <div className="tr-val-ind">
-              <span className="data-item-title">حد أدنى عادي شركات:</span>
-              <span className="data-item-value">{cropData.min_val_cor}</span>
-            </div>
-          )}
+            {cropData.min_val_cor && (
+              <div className="tr-val-ind">
+                <span className="data-item-title">حد أدنى عادي شركات:</span>
+                <span className="data-item-value">{cropData.min_val_cor}</span>
+              </div>
+            )}
 
-          {cropData.max_val_cor && (
-            <div className="tr-val-ind">
-              <span className="data-item-title">حد أقصى عادي شركات:</span>
-              <span className="data-item-value">{cropData.max_val_cor}</span>
-            </div>
-          )}
+            {cropData.max_val_cor && (
+              <div className="tr-val-ind">
+                <span className="data-item-title">حد أقصى عادي شركات:</span>
+                <span className="data-item-value">{cropData.max_val_cor}</span>
+              </div>
+            )}
 
-          {cropData.begin && (
-            <div className="begin-date">
-              <span className="data-item-title">بداية المنح:</span>
-              <span className="data-item-value">{cropData.begin}</span>
-            </div>
-          )}
+            {cropData.begin && (
+              <div className="begin-date">
+                <span className="data-item-title">بداية المنح:</span>
+                <span className="data-item-value">{cropData.begin}</span>
+              </div>
+            )}
 
-          {cropData.end && (
-            <div className="end-date">
-              <span className="data-item-title">نهاية المنح:</span>
-              <span className="data-item-value">{cropData.end}</span>
-            </div>
-          )}
+            {cropData.end && (
+              <div className="end-date">
+                <span className="data-item-title">نهاية المنح:</span>
+                <span className="data-item-value">{cropData.end}</span>
+              </div>
+            )}
 
-          {cropData.due && (
-            <div className="due">
-              <span className="data-item-title">تاريخ الاستحقاق:</span>
-              <span className="data-item-value">{cropData.due}</span>
-            </div>
-          )}
+            {cropData.due && (
+              <div className="due">
+                <span className="data-item-title">تاريخ الاستحقاق:</span>
+                <span className="data-item-value">{cropData.due}</span>
+              </div>
+            )}
 
-          {cropData.inst_no && (
-            <div className="inst-number">
-              <span className="data-item-title">كتاب دوري رقم:</span>
-              <span className="data-item-value">{cropData.inst_no}</span>
-            </div>
-          )}
+            {cropData.inst_no && (
+              <div className="inst-number">
+                <span className="data-item-title">كتاب دوري رقم:</span>
+                <span className="data-item-value">{cropData.inst_no}</span>
+              </div>
+            )}
 
-          {cropData.inst_dte && (
-            <div className="inst-date">
-              <span className="data-item-title">بتاريخ:</span>
-              <span className="data-item-value">{cropData.inst_dte}</span>
-            </div>
-          )}
+            {cropData.inst_dte && (
+              <div className="inst-date">
+                <span className="data-item-title">بتاريخ:</span>
+                <span className="data-item-value">{cropData.inst_dte}</span>
+              </div>
+            )}
 
-          {cropData.notes && (
-            <div className="notes">
-              <span className="data-item-title">ملاحظات:</span>
-              <span className="data-item-value">{cropData.notes}</span>
-            </div>
-          )}
+            {cropData.notes && (
+              <div className="notes">
+                <span className="data-item-title">ملاحظات:</span>
+                <span className="data-item-value">{cropData.notes}</span>
+              </div>
+            )} </div>)}
+
         </div>
       )}
     </main>
